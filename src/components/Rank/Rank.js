4eb112759494c8c8ad936ff0f9acb7a1c0bf11b3
Nathaniel Angelico Tabanao <nathanielangelico23@gmail.com>
2020-09-05 23:33:24 +0800

    Navigation & Rank

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+const Rank = () => {
+    return (
+        <div className=''>
+            <div className='white f3'>
+                {'Nate your current rank is'}
+            </div>
+            <div className='white f1'>
+                {'#5'}
+            </div>
+        </div>
+    )
+}
+
+export default Rank;
