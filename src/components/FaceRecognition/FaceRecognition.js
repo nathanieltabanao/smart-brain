7cc708c604664ca1457663c841eda75e6b9db215
Nathaniel Angelico Tabanao <nathanielangelico23@gmail.com>
2020-09-05 23:20:06 +0800

    Face recognition & Imagelink Forms

---
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './FaceRecognition.css';
+
+const FaceRecognition = ({imageUrl,box}) => {
+    console.log(box, 'face');
+
+    return (
+        <div className='center ma'>
+            <div className='absolute mt2'>
+                <img
+                    id='inputimage' 
+                    width='500px'
+                    height='auto'
+                    alt='Test'
+                    src={imageUrl} />
+                <div className='bounding-box' 
+                    style={{top: box.topRow,
+                        right: box.rightCol,
+                        bottom: box.bottomRow,
+                        left: box.leftCol}}></div>
+            </div>
+        </div>
+    )
+}
+
+export default FaceRecognition;
